b6d1f7c899437807a4fc412a96f4c175d2478648
Ian <iancabello16@gmail.com>
2023-07-17 02:53:26 -0400

    create 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,28 @@
+import './scss/not-found.scss';
+
+import Nav from './components/Nav';
+import Link from 'next/link';
+
+export default function NotFound() {
+  return (
+    <>
+      <Nav />
+      <div className='error_logo_container'>
+        <img src='/images/logo.svg' className='error_logo' alt='Adopt Local Hopatcong Animal Control' width={300} />
+      </div>
+      <div className='error_page'>
+        <img src='/images/dog-eyes-1.jpg' className='error_image' alt='Puppy covering eyes' width={300} />
+        <span>
+          <h2>OH NO! It looks like you're lost!</h2>
+        </span>
+        <button className='error_home_button'>
+          <h1>
+            <Link href='/' className='error_button_text'>
+              Take Me Home!
+            </Link>
+          </h1>
+        </button>
+      </div>
+    </>
+  );
+}
